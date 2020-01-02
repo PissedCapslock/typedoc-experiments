7298b2a5c202f5ef76f8005b4f73a504fbeef801
STEVENS Robin <stevensro@gmail.com>
2020-01-02 15:43:25 +0100

    Ignored the generated tsdoc

--- tsdoc/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"1":"External module"},"rows":[{"id":0,"kind":1,"name":"\"Foo.d\"","url":"modules/_foo_d_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":1,"name":"\"Foo\"","url":"modules/_foo_.html","classes":"tsd-kind-external-module"}]};
